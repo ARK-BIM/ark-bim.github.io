995f63d431e178b3df369cbdfe280096359a604e
ARK-BIM <85930932+ARK-BIM@users.noreply.github.com>
2021-07-14 19:44:35 +0200

    Update fcf.config.php

--- fcf-assets/fcf.config.php
+++ fcf-assets/fcf.config.php
@@ -83,7 +83,7 @@ define('EMAIL_TO_CC_NAME', '');
 define('EMAIL_TO_BCC', '');
 define('EMAIL_TO_BCC_NAME', '');
 
-define('EMAIL_FROM', '');
+define('EMAIL_FROM', 'arkbim.info@gmail.com');
 define('EMAIL_FROM_NAME', '');
 
 define('EMAIL_REPLY_TO', 'FIELD:Email');
